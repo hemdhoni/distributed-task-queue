55ea4608a2ef83c50c23dfde40e8e2c1a748d000
hem dhoni <hemdhoni@hems-MacBook-Air.local>
2026-07-02 17:25:32 +0530

    change redis code

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,16 +1,17 @@
 const IORedis = require("ioredis");
 
 const redis = new IORedis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  maxRetriesPerRequest: null,
 });
 
 redis.on("connect", () => {
-    console.log("Redis Connected");
+  console.log("Redis Connected");
 });
 
 redis.on("error", (err) => {
-    console.log(err);
+  console.error("Redis Error:", err);
 });
 
 module.exports = redis;
